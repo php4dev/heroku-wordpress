18093bc8b486cdc6817e349c6ebdf1e6a06245ab
Valentyn Kolesnikov <javadev75@gmail.com>
2019-01-05 19:02:26 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -81,5 +81,9 @@ define('WP_DEBUG', false);
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && 'https' == $_SERVER['HTTP_X_FORWARDED_PROTO'] ) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
