d724028bada2bb2e2efbf629521d2bbc98c803a0
javadev <javadev75@gmail.com>
2019-01-06 15:22:35 +0200

    Remove vendor.

--- vendor/heroku/heroku-buildpack-php/conf/nginx/default_include.conf.php
+++
@@ -1,8 +0,0 @@
-location / {
-	index  index.php index.html index.htm;
-}
-
-# for people with app root as doc root, restrict access to a few things
-location ~ ^/(composer\.(json|lock|phar)$|Procfile$|<?=getenv('COMPOSER_VENDOR_DIR')?>/|<?=getenv('COMPOSER_BIN_DIR')?>/) {
-	deny all;
-}
