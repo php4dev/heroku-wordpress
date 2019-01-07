67db0788d9dd8865d6f682bb5c4be9f5757e0985
javadev <javadev75@gmail.com>
2019-01-07 10:37:10 +0200

    Change upload file limit.

--- wp-content/themes/storefront/functions.php
+++ wp-content/themes/storefront/functions.php
@@ -5,6 +5,9 @@
  * @package storefront
  */
 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 /**
  * Assign the Storefront version to a var
  */
