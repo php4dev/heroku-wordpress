7c2eace6843a3374afd7bbd22cffe1883fc20be7
Valentyn Kolesnikov <javadev75@gmail.com>
2019-02-23 14:04:02 +0100

    Switch to pgsql.

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,7 @@ define('SENDGRID_API_KEY', $_ENV["SENDGRID_API_KEY"]);
 
 define('ALTERNATE_WP_CRON', true);
 // ** Heroku Postgres settings - from Heroku Environment ** //
-$db = parse_url($_ENV["JAWSDB_MARIA_URL"]);
+$db = parse_url($_ENV["DATABASE_URL"]);
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
