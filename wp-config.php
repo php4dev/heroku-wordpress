b74c2e1cfe0fb169f7c6aabbaf7be05c45e6e5b0
Louis_0723 <lursun@cybavo.com>
2021-01-06 17:39:09 +0800

    debug

--- wp-config.php
+++ wp-config.php
@@ -75,7 +75,7 @@ $table_prefix  = 'wp_';
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
